6206b4bde6e39f38491fc53b302fdfb7b433e476
JhosuaArias <jhosuarqui@hotmail.com>
2017-10-22 15:56:22 -0600

    Add Server and Client Tests

---
+++ src/Server_Client_Test/ClientTest.java
@@ -0,0 +1,36 @@
+package Server_Client_Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientTest {
+
+    public static void main(String args[]) throws IOException {
+        final String host = "localhost";
+        final int portNumber = 81;
+        System.out.println("Creating socket to '" + host + "' on port " + portNumber);
+
+        while (true) {
+            Socket socket = new Socket(host, portNumber);
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            System.out.println("server says:" + br.readLine());
+
+            BufferedReader userInputBR = new BufferedReader(new InputStreamReader(System.in));
+            String userInput = userInputBR.readLine();
+
+            out.println(userInput);
+
+            System.out.println("server says:" + br.readLine());
+
+            if ("exit".equalsIgnoreCase(userInput)) {
+                socket.close();
+                break;
+            }
+        }
+    }
+}
