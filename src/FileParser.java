72e69ad6e461b0b31fa9acb624622cd58bc11a39
JhosuaArias <jhosuarqui@hotmail.com>
2017-10-22 17:02:14 -0600

    File Parser

--- src/FileParser.java
+++ src/FileParser.java
@@ -1,3 +1,55 @@
-public class FileParser {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class FileParser {
+    public As createAS(String fileName) throws Exception {
+        As createdAs = null;
+
+        int asId;
+        int asPort;
+        String asIp;
+        ArrayList<String> knownSubnetworks = new ArrayList<>();
+        ArrayList<String> bgpNetworks = new ArrayList<>();
+        int listenNeighbors;
+
+        File file = new File(fileName);
+
+        Scanner input = new Scanner(file);
+
+        while(input.hasNext()) {
+            int index = -1;
+            String nextLine = input.nextLine();
+            if(nextLine.contains("#")) {
+                index++;
+            } else {
+                switch (index) {
+                    case 0: //id
+                        asId = Integer.parseInt(nextLine);
+                        break;
+                    case 1: //port
+                        asPort = Integer.parseInt(nextLine);
+                        break;
+                    case 2: //ip
+                        asIp = nextLine;
+                        break;
+                    case 3: //known networks
+                        knownSubnetworks.add(nextLine);
+                        break;
+                    case 4: //BGP neighbors
+                        bgpNetworks.add(nextLine);
+                        break;
+                    case 5: //listen neigbors
+                        listenNeighbors = Integer.parseInt(nextLine);
+                        break;
+                }
+            }
+        }
+
+        //createdAs = new As(asId,asPort,asIp,knownSubnetworks,bgpNetworks,listenNeighbors);
+
+        input.close();
+
+        return createdAs;
+    }
 }
