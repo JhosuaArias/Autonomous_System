18a5c037a3229a709d3256f92bccd1e906bf1d32
danielmdob <danielmdob@users.noreply.github.com>
2017-10-22 18:56:41 -0600

    Terminal y controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,54 @@
-public class Controller {
+import com.sun.deploy.util.StringUtils;
+
+public class Controller implements Runnable {
+
+    //AS autonomousSystem;
+
+    Controller(String fileName) {
+        //FileParser parser = new FileParser();
+        //this.autonomousSystem= parser.createAS();
+    }
+
+    private void listenToTerminal() {
+        Terminal terminal = new Terminal();
+        String command;
+        while(!(command = terminal.receiveCommand()).equals("stop")) {
+            command = command.trim().replaceAll(" +", " ");  // removes unwanted whitespace
+            parseCommand(command);
+        }
+    }
+
+    private void parseCommand(String command) {
+        if (command.equals("help")) {
+            this.helpCommand();
+        } else if (command.equals("start")) {
+            this.startCommand();
+        } else if(command.startsWith("add ") && command.split(" ").length == 2) { //whitespace at the end to ensure that a parameter is present (string is trimmed)
+            this.addSubnetCommand(command.split(" ")[1]);  //sends only the address as parameter
+        } else if (command.equals("show routes")) {
+            this.showRoutesCommand();
+        } else {
+            System.out.println("Unsupported command or wrong quantity of parameters, use the \"help\" command if needed");
+        }
+    }
+
+    public void run() {
+        this.listenToTerminal();
+    }
+
+    private void startCommand() {
+        //this.autonomousSystem.start();
+    }
+
+    private void helpCommand() {
+        System.out.println("The supported commands are: \n -help \n -start \n -stop \n -show routes \n -add <<subnet address>>");
+    }
+
+    private void showRoutesCommand() {
+        //this.autonomousSystem.showRoutes();
+    }
+
+    private void addSubnetCommand(String subnet) {
+        //this.autonomousSystem.addSubNetwork();
+    }
 }
--- src/Terminal.java
+++ src/Terminal.java
@@ -1,38 +1,22 @@
-import java.util.StringTokenizer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Terminal {
+    BufferedReader stdIn;
 
-    private As as;
-
-    private final String ADDRESS_FORMAT = "(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])";
-
-    public Terminal() {
-        As as = new As();
+    Terminal() {
+        stdIn = new BufferedReader(new InputStreamReader(System.in));
     }
 
-    public void consoleTokenizer(String command) {
-        StringTokenizer stringTokenizer = new StringTokenizer(command);
-        if (command.compareTo("start") == 0) {
-            as.start();
-        } else if (command.compareTo("stop") == 0) {
-            as.stop();
-        } else if (command.compareTo("show routes") == 0) {
-            as.showRoutes();
-        } else if (command.compareTo("help") == 0) {
-            as.help();
-        } else if (stringTokenizer.nextToken().compareTo("add") == 0) {
-            String address = stringTokenizer.nextToken();
-            Pattern pattern = Pattern.compile(ADDRESS_FORMAT);
-            Matcher matcher = pattern.matcher(address);
-            if (matcher.matches()) {
-                as.addSubNetwork(address);
-            } else {
-                System.out.println("The given address is not valid");
-            }
-        } else {
-            System.out.println("Unknown command");
+    String receiveCommand() {
+        String userInput = "";
+        try {
+            userInput = stdIn.readLine();
+        } catch (IOException e) {
+            System.out.println("IOException, could not read from std in");
         }
+        return userInput;
     }
 }
