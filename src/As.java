fab9752a19672669bb8fea2abe1cbdbcf75b7989
Elías Calderón <eliascr98@outlook.com>
2017-10-24 11:51:41 -0600

    Pre alpha of AS

--- src/As.java
+++ src/As.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class As {
-    
 
-    public As() {
-        
+    private int id;
+    private int port;
+    private ArrayList<String> knownSubnetworks;
+    private HashMap<String, String> bgpNeighbors;
+    private RoutingTable routingTable;
+
+    public As(int id, int port, ArrayList<String> knownSubnetworks, HashMap<String, String> bgpNeighbors) {
+        this.id = id;
+        this.port = port;
+        this.knownSubnetworks = knownSubnetworks;
+        this.bgpNeighbors = bgpNeighbors;
+
+        this.routingTable = new RoutingTable();
+        this.routingTable.addRoute("192.168.1.2", new String[]{"AS1", "AS2", "AS3"});
+        this.routingTable.addRoute("192.168.1.2", new String[]{"AS1", "AS2", "AS3", "AS4", "AS5"});
+        this.routingTable.addRoute("192.168.1.2", new String[]{"AS1", "AS2", "AS3", "AS7"});
+        this.routingTable.addRoute("192.168.1.2", new String[]{"AS1", "AS6", "AS5"});
+        this.routingTable.sort();
+        this.routingTable.print();
     }
 
     public void start() {
---
+++ src/TestRoute.java
@@ -0,0 +1,8 @@
+
+public class TestRoute {
+
+    public static void main(String[] args) {
+        As testAS = new As(1, 5555, null, null);
+    }
+
+}
